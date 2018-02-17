0f5d28ebdc292c2a867c2a109d8fc4928982ef7b
zjjc123 <zjjc123@hotmail.com>
2018-02-16 17:40:11 -0800

    Another Fix

--- In-the-zone/src/opcontrol.c
+++ In-the-zone/src/opcontrol.c
@@ -19,9 +19,9 @@ int getJoystick(unsigned char joystick, unsigned char axis){
 		value = 0;
 	}
 	if (value > 0){
-		value = (value * value) / 16129;
+		value = (value * value) / 127;
 	} else{
-		value = (value * value) / -16129;
+		value = (value * value) / -127;
 	}
 
 	return value;
