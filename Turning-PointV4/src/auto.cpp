b2e9ac3addb15a15b592b768515cecae720bd161
zjjc123 <zjjc123@hotmail.com>
2018-12-07 17:09:44 -0800

    508

--- Turning-PointV4/src/auto.cpp
+++ Turning-PointV4/src/auto.cpp
@@ -9,7 +9,7 @@
  *
  * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
  * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
- */
+ */12/7
 
 #include "cycle.h"
 #include "actions.h"
