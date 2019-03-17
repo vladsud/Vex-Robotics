df0957ae5b37960c27e761a6e4b27b030416fba8
Vlad <vlad@sudzilouski.com>
2019-03-17 15:54:29 -0700

    Fixing LCD code, leveraging legacy APIs

--- Turning-PointV5/src/lcd.cpp
+++ Turning-PointV5/src/lcd.cpp
@@ -4,22 +4,22 @@
 #include "battery.h"
 #include "aton.h"
 
+#include "pros/api_legacy.h"
+
 LCD::LCD()
 {
     m_buttons = 0;
     m_step = 0;
 
-    lcdInit(uart1);
-    lcdClear(uart1);
-    lcdSetBacklight(uart1, true);
+    lcdInit();
+    lcdClear();
     PrintStepInstructions();
     m_count = 0;
 }
 
 void LCD::PrintMessage(const char *message)
 {
-    lcdSetBacklight(uart1, true);
-    lcdSetText(uart1, 1, message);
+    lcdSetText(1, message);
 }
 
 void LCD::PrintStepInstructions()
@@ -29,42 +29,42 @@ void LCD::PrintStepInstructions()
     switch (m_step)
     {
     case 0:
-        lcdSetText(uart1, 1, "Main Battery");
+        lcdSetText(1, "Main Battery");
 
-        lcdPrint(uart1, 2, "%.2f  Cont", mp);
+        lcdPrint(2, "%.2f  Cont", mp);
 
         break;
     case 1:
         if (AtonSkills)
         {
-            lcdSetText(uart1, 1, "    SKILLS!    ");
-            lcdSetText(uart1, 2, "     Cancel     ");
+            lcdSetText(1, "    SKILLS!    ");
+            lcdSetText(2, "     Cancel     ");
         }
         else
         {
             // Choose side
-            lcdSetText(uart1, 1, "Left     Right");
-            lcdPrint(uart1, 2, "Red  Skill  Blue");
+            lcdSetText(1, "Left     Right");
+            lcdSetText(2, "Red  Skill  Blue");
         }
         break;
     case 2:
         // Select position
-        lcdSetText(uart1, 1, "First Pos?");
+        lcdSetText(1, "First Pos?");
 
-        lcdSetText(uart1, 2, "No    Back   Yes");
+        lcdSetText(2, "No    Back   Yes");
         break;
     case 3:
         // Select platform
-        lcdSetText(uart1, 1, "Climb platform?");
+        lcdSetText(1, "Climb platform?");
 
-        lcdSetText(uart1, 2, "No    Back   Yes");
+        lcdSetText(2, "No    Back   Yes");
         break;
     case 4:
         // Print auton
-        lcdPrint(uart1, 1, "%s, %s",
+        lcdPrint(1, "%s, %s",
                  AtonBlueRight ? "Blue" : "Red",
                  AtonFirstPos ? "1st" : "2nd");
-        lcdPrint(uart1, 2, "%-8s  Cancel",
+        lcdPrint(2, "%-8s  Cancel",
                  AtonClimbPlatform ? "Climb" : "No climb");
         break;
     }
@@ -100,20 +100,19 @@ void LCD::Update()
     m_count++;
     if (m_step == 0 && m_count % 100 == 0)
     {
-        Battery bat;
         float mp = GetMainPower();
-        //lcdSetText(uart1, 1, "Main        ");
-        lcdPrint(uart1, 2, "%.2f  Cont", mp);
+        //lcdSetText(1, "Main        ");
+        lcdPrint(2, "%.2f  Cont", mp);
 
-        if (ep > 6.0f && mp > 6.0f)
+        if (mp > 10.0f)
         {
             m_disable = false;
             PrintStepInstructions();
         }
         else
         {
-            lcdSetText(uart1, 1, "Battery");
-            lcdSetText(uart1, 2, "NOT Connected");
+            lcdSetText(1, "Battery");
+            lcdSetText(2, "NOT Connected");
             m_disable = true;
         }
     }
@@ -123,7 +122,7 @@ void LCD::Update()
 
     // Read button
     // Returns a 3 bit integer: 100 is left, 010 is center, 001 is right
-    int buttons = lcdReadButtons(uart1);
+    int buttons = lcdReadButtons();
 
     // If the button is the same (still pressing), ignore actions
     if (m_buttons == buttons)
