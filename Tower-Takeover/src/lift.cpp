00140e117ba6c943bbc90256c5edd37da8d189a5
Rodahan <rohankrishnan@live.com>
2019-11-10 21:32:24 -0800

    Semi Functional Lift PID

--- Tower-Takeover/src/lift.cpp
+++ Tower-Takeover/src/lift.cpp
@@ -20,15 +20,44 @@ void Lift::SetLiftMotor(int speed)
 
 void Lift::Update()
 {
+
+    int kP = 25;
+    int kI = 8000;
+
+    // Target value
+    int armValue = 2800;
+    int trayValue = 2100;
+
+    int currentArm = m_anglePot.get_value();
+    int currentTray = GetMain().cubetray.m_anglePot.get_value();
+
     // Up
     if (joystickGetDigital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_X))
     {
+        
         SetLiftMotor(90);
+
+        int currTrayError = currentTray - trayValue;
+        int currArmError = currentArm - armValue;
+        totalTrayError += currTrayError;
+        totalArmError += currArmError;
+
+        int currTraySpeed = currTrayError / kP + totalTrayError / kI;
+        motor_move(cubetrayPort, -currTraySpeed);
+        
+        int currArmSpeed = currArmError / kP + totalArmError / kI;
+        SetLiftMotor(currArmSpeed);
     }
     // Down
     else if (joystickGetDigital(E_CONTROLLER_MASTER, E_CONTROLLER_DIGITAL_B))
     {
         SetLiftMotor(-90);
+
+        if (currentRotation < 2850) {
+            motor_move(cubetrayPort, 127);
+        } else {
+            motor_move(cubetrayPort, 0);
+        }
     }
     else
     {
